9436f6db85aa68d52e1caff916d1cf862a03a2e9
rgrannell1 <r.grannell1@nuigalway.ie>
2013-08-21 00:47:03 +0100

    added xIsEmpty, tests, benchmark

---
+++ R/xIsEmpty.R
@@ -0,0 +1,20 @@
+
+#' Test if a collection is empty.
+#'
+#' @param collection a list, pairlist, or vector of arbitrary values.
+#'
+#' @return A true or false value.
+#'
+#' @export
+
+#| function: xIsEmpty version: 0.1 finished: false
+
+xIsEmpty <- function (collection) {
+	# Collection -> boolean
+	# is collection length == 0?
+
+	pcall <- sys.call()
+	require_a("listy", collection, pcall)
+
+	length(collection) == 0
+}
---
+++ inst/benchmarks/bench-xIsEmpty.R
@@ -0,0 +1,2 @@
+
+bench$xIsEmpty <- xIsEmpty
