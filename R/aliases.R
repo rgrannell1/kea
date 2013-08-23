b68ec91265774811b3ab7ce0af6a6d062515d8b1
rgrannell1 <r.grannell1@nuigalway.ie>
2013-08-23 06:15:16 +0100

    added basic implementation of xCompose

--- R/aliases.R
+++ R/aliases.R
@@ -6,11 +6,21 @@ True <- TRUE
 False <- FALSE
 
 # to complete the following triad in the cleanest way;
-# if ( True ); if ( !False ), if( ._(Unknown) )
-# hopefully bquote() doesn't get sick over my code.
+# if ( True ); if ( !False ), if( ._(Na) )
 
 ._ <- is.na
 
 object <- function () {
 	new.env(parent = emptyenv())
+}
+
+call_with_params <- function (name, fn) {
+	# string -> function -> call
+	# create call for a function with
+	# the arguments of another function.
+
+	as.call(
+		lapply(
+			c(name, names(formals(fn)) ),
+			as.symbol))
 }
---
+++ R/xCompose.R
@@ -0,0 +1,43 @@
+
+#' Compose two unary functions.
+#'
+#' @param f a unary function, or a 
+#'     symbol or name identifying such a function.
+#' @param g a unary function, or a 
+#'     symbol or name identifying such a function.
+#'
+#' @return A function with the same parameters as \code{g}.
+#'
+#' @export
+#'
+
+#| function: xCompose version: 0.1 finished: false 
+
+xCompose <- function (fn_1, fn_2) {
+	# (a -> b) -> (b -> c) -> (a -> c)
+	# return a composite function f o g.
+	# constructed using a macro.
+	
+	pcall <- sys.call()
+	pframe <- parent.frame()
+
+	require_a("functionable", fn_1, pcall)
+	require_a("functionable", fn_2, pcall)
+	
+	fn_1 <- match.fun(fn_1)
+	fn_2 <- match.fun(fn_2)
+		
+	require_a("unary function", fn_1, pcall)
+	remove(pcall, pframe)
+
+	do.call("function", list(
+		formals(fn_2),
+		bquote({
+			fn_1(.( call_with_params("fn_2", fn_2) ))
+		})
+	))
+}
+
+#' @export
+
+'%of%' <- xCompose
