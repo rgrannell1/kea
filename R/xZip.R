bd3b512cf7dc63321970cc06e8d589cd86bdb784
rgrannell1 <r.grannell1@nuigalway.ie>
2013-08-23 23:18:57 +0100

    added xZip, not tests because its a curried form of xZipWith

---
+++ R/xZip.R
@@ -0,0 +1,16 @@
+
+#' Generate a list of n-element lists from n collections.
+#' 
+#' @param ... n-vectors, lists or pairlists.
+#'
+#' @return returns a list of equal length to the shortest input collection, with each element being
+#'     an n-element list.
+#' @section Corner Cases:
+#'      the empty list is returned if the shortest collection has length-zero, or no collections
+#'      are included. Each collection is truncated to the length of the shortest collection.
+#'
+#' @export
+
+xZip <- function(...) {
+	xZipWith(function (...) list(...), ...)
+}
