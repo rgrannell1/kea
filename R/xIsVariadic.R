76c68701a520f699a84af02dd1376234c93cc697
rgrannell1 <r.grannell1@nuigalway.ie>
2013-08-24 00:15:11 +0100

    added xIsVariadic, tests.

---
+++ R/xIsVariadic.R
@@ -0,0 +1,26 @@
+
+#' Is a function variadic?
+#'
+#' @param fn an arbitrary function, or a
+#'     symbol or string identifying such a function.
+#'
+#' @return a list of true of false value of the same length as the 
+#'     arity of \code{fn}.
+#'
+#' @section Corner Cases:
+#'     If \code{fn} is a nullary function the result is false.
+#' @family arrow-parameters
+#' @export
+
+#| function: xIsVariadic version: 0.1 finished: true
+
+xIsVariadic <- function (fn) {
+	# (a -> b) -> [boolean]
+	# is ... in fn's parameters?
+
+	pcall <- sys.call()
+	require_a("functionable", fn, pcall)
+	f <- match.fun(fn)
+
+	"..." %in% names(xFormals(fn))
+}
---
+++ inst/tests/test-xIsVariadic.R
@@ -0,0 +1,11 @@
+
+context("xIsVariadic")
+
+test_that("xIsVariadic", {
+
+	expect_equal(xIsVariadic(function (x) x), False)
+	expect_equal(xIsVariadic(function () {}), False)
+	expect_equal(xIsVariadic(function (...) x), True)
+	expect_equal(xIsVariadic(function (x, ...) x), True)
+
+})
