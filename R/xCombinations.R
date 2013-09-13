3fbdbca695a0f70c01afb576593c8153b619fdb5
rgrannell1 <r.grannell1@nuigalway.ie>
2013-09-13 01:33:45 +0100

    added xCombinations

---
+++ R/xCombinations.R
@@ -0,0 +1,36 @@
+
+#' xCombinations
+#' 
+#' Generate all ways of choosing several elements from a column.
+#'
+#' @param num a nonnegative whole number.
+#' @param coll a collection
+#'
+#' @return a list of lists, with each list containing \code{num} elements.
+#'
+#' @section Corner Cases: 
+#'     returns the empty list if \code{num} is zero.
+#' @template glossary
+#'
+#' @examples 
+#' @export
+
+xCombinations <- function (num, coll) {
+	# number -> Collection
+
+	pcall <- sys.call()
+
+	assert(length(num) == 1, pcall)
+	assert(is.numeric(num) && num >= 0, pcall)
+	assert(round(num) == num, pcall)
+
+	assert(
+		is.vector(coll) || is.pairlist(coll), pcall)	
+
+	if (num == 0) {
+		list()
+	} else {
+		num <- min(length(coll), num)
+		apply(combn(coll, num), 2, as.list)			
+	}
+}
