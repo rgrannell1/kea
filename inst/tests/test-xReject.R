53b747d96268a365e38f3d3d74f76876b2476463
rgrannell1 <r.grannell2@gmail.com>
2013-10-21 16:23:40 +0100

    added tests for xReject

--- inst/tests/test-xReject.R
+++ inst/tests/test-xReject.R
@@ -1,21 +1,49 @@
 
-context("xReject")
+context("xReject: positive controls")
 
-test_that("xReject", {
+forall(
+	"the empty collection always yields the empty list.",
+	list(fn = G$logical_functions, coll = G$collection_zero),
+	xReject(fn, coll) %equals% list()
+)
 
-	odds <- function (x) {
-		x %% 2 == 0
-	}
+forall(
+	"a truth function is list unit for collection.",
+	G$standard$truth_with_coll(),
+	expect = 
+		xReject(fn, coll) %equals% list(),
+	given = 
+		length(coll) > 0
+)
 
-	expect_that(
-		xReject(Truth, pairlist()), equals(list()) )
-	expect_that(
-		xReject(Truth, list(1, 2, 3)), equals(list()) )
-	expect_that(
-		xReject(Falsity, list(1, 2, 3)), equals(list(1, 2, 3)) )
-	expect_that(
-		xReject(odds, as.list(1:6)), equals(list(1, 3, 5)) )
-	expect_that(
-		xReject(odds, 1:6), equals(list(1, 3, 5)) )
-	
-})
+forall(
+	"a falsity function is list identity for collection.",
+	G$standard$falsity_with_coll(),
+	xReject(fn, coll) %equals% coll
+)
+
+forall(
+	"a na function is list identity for collection.",
+	G$standard$mu_with_coll(),
+	xReject(fn, coll) %equals% as.list(coll)
+)
+
+forall(
+	"selecting the odd-numbers works as expected, and ordering is preserved.",
+	G$standard$mod2_over_ints(),
+	xReject(fn, coll) %equals% as.list(coll[coll %% 2 == 1])
+)
+
+forall(
+	"collection.xReject selects odd-numbers.",
+	G$standard$mod2_over_ints(),
+	x_(coll)$xReject(fn)$x() %equals% 
+		as.list(coll[coll %% 2 == 1])
+)
+
+forall(
+	"function.xReject selects odd-numbers.",
+	G$standard$mod2_over_ints(),
+	x_(fn)$xReject(coll)$x() %equals% 
+		as.list(coll[coll %% 2 == 1])
+)
