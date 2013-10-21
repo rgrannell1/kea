c08e601d52db34f5d37a9b0922220482d27b7374
rgrannell1 <r.grannell2@gmail.com>
2013-10-21 21:00:53 +0100

    added more tests for map and mapalong

--- inst/tests/test-xMap.R
+++ inst/tests/test-xMap.R
@@ -10,14 +10,18 @@ forall(
 forall(
 	"mapping identity over the list preserves its contents & length.",
 	list(coll = G$collection),
-	xMap(identity, coll) %equals% as.list(coll) &&
-	length(xMap(identity, coll)) == length(coll)
+	{
+		xMap(identity, coll) %equals% as.list(coll) &&
+		length(xMap(identity, coll)) == length(coll)
+	}
 )
 
 forall(
 	"mapping increment increments the list",
 	G$standard$inc_over_ints(),
-	all( unlist(xMap(fn, coll)) == unlist(coll) + 1 )
+	{
+		all( unlist(xMap(fn, coll)) == unlist(coll) + 1 )
+	}
 )
 
 context("arrow $ xMap: positive controls")
@@ -25,12 +29,15 @@ context("arrow $ xMap: positive controls")
 forall(
 	"collection.xMap selects even-numbers.",
 	G$standard$inc_over_ints(),
-	all( unlist(x_(coll)$xMap(fn)$x()) == unlist(coll) + 1 )
+	{
+		all( unlist(x_(coll)$xMap(fn)$x()) == unlist(coll) + 1 )
+	}
 )
 
 forall(
 	"function.xMap selects even-numbers.",
 	G$standard$inc_over_ints(),
-	all( unlist(x_(fn)$xMap(coll)$x()) == unlist(coll) + 1 )
+	{
+		all( unlist(x_(fn)$xMap(coll)$x()) == unlist(coll) + 1 )
+	}
 )
-
--- inst/tests/test-xMapAlong.R
+++ inst/tests/test-xMapAlong.R
@@ -1,16 +1,46 @@
 
-context("xMapAlong")
+context("xMapAlong: positive controls")
 
-test_that("xMapAlong", {
+forall(
+	"mapalong the empty collection always yields the empty list.",
+	list(
+		fn = function () {
+			function (val, ind) val + ind 
+		}, 
+		coll = G$collection_zero
+	),
+	xMapAlong(fn, coll) %equals% list()
+)
 
-	expect_that(
-		xMapAlong(function (x, i) i, 3:1),
-		equals(list(1, 2, 3))
-	)
+forall(
+	"mapalong's indices are correct",
+	list(
+		fn = function () {
+			function (val, ind) ind
+		},
+		coll = G$collection
+	),
+	xMapAlong(fn, coll) %equals% as.list(seq_along(coll))
+)
 
-	expect_that(
-		xMapAlong(function (x, i) x, 3:1),
-		equals(list(3, 2, 1))
-	)
+forall(
+	"mapalong's values are correct",
+	list(
+		fn = function () {
+			function (val, ind) val
+		},
+		coll = G$collection
+	),
+	xMapAlong(fn, coll) %equals% as.list(coll)
+)
 
-})
+forall(
+	"mapalong's can increment correctly.",
+	list(
+		fn = function () {
+			function (val, ind) val + 1
+		},
+		coll = G$integers()
+	),
+	all( unlist(xMapAlong(fn, coll)) == unlist(coll) + 1 )
+)
