066a6ac98ba1544cd0bb072b66b0d906bac46749
rgrannell1 <r.grannell2@gmail.com>
2013-11-02 21:21:53 +0000

    corrected tests for collapse

--- tests/test-xCollapse.R
+++ tests/test-xCollapse.R
@@ -1,10 +1,27 @@
 
 message('xCollapse')
 
-test_that('xCollapse', {
+forall(
+	"collapsing with character() is the same as collapsing with ''",
+	list(words = G$words()),
+	{
 
-	expect_equal(xCollapse('', ''), '')
-	expect_equal(xCollapse('', c('a', 'b')), 'ab')
-	expect_equal(xCollapse('|', c('a', 'bc')), 'a|bc')
+		do.call( xCollapse, c(str = "", list(words)) ) %equals%
+		do.call( xCollapse, c(str = list(character()), list(words)) )
+	}
+)
 
-})
+forall(
+	"collapsing character() and '' acts as identity ",
+	list(delim = G$word(), words = G$words()),
+	{
+		stripped_words <-
+			Filter(function (x) nchar(x) > 0 && length(x) > 0, words)
+
+		do.call( xCollapse, c(delim, list(words)) ) %equals%
+		do.call( xCollapse, c(delim, list(stripped_words)) )
+	}
+)
+
+message('Arrow $ xCollapse')
+
