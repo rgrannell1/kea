4b0c187aeac5acef8b8dbacfe20e78c92bca652e
rgrannell1 <r.grannell2@gmail.com>
2013-11-02 20:24:05 +0000

    added tests for xIterate

--- tests/test-xIterate.R
+++ tests/test-xIterate.R
@@ -1,5 +1,21 @@
 
 message("xIterate")
 
+forall(
+	"Return is the identity function",
+	list(init = G$integers()),
+	xIterate(function (n) Return(n), init) %equals% init
+)
+
+forall(
+	"incrementing to a value works",
+	list(upper = G$positive()),
+	xIterate(
+		function (n) {
+			if (n == upper) Return(n) else n + 1
+		},
+		0) %equals% upper
+)
 
 message("arrow $ xIterate")
+
