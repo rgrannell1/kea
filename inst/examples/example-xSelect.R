caf6403b71bd4eafb5fcd2a356e1ba7908dcf9d1
rgrannell1 <r.grannell2@gmail.com>
2013-11-09 20:09:15 +0000

    added xsekect examples

---
+++ inst/examples/example-xSelect.R
@@ -0,0 +1,39 @@
+
+# select the even numbers.
+
+xSelect(n := {n %% 2 == 0}, 1:5)
+
+list(2L, 4L)
+
+# select names matching a regular expression
+
+xSelect(
+	name := {
+		grepl("_", name)
+	},
+	c("Church", "Turing", "Godel"))
+
+list("Church", "Turing")
+
+# select employees over 30
+
+xSelect(
+	record := {
+		record$age > 30
+	},
+	list(
+		list(
+			name = "Jack Thompson", age = 19),
+		list(
+			name = "Employee #745", age = 55),
+		list(
+			name = "Cog #234", age = 57)
+	)
+)
+
+list(
+	list(
+		name = "Employee #745", age = 55),
+	list(
+		name = "Cog #234", age = 57)
+)
