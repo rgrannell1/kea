43cfc4bbdabc2d2be67c7f944fbf02eace28292a
rgrannell1 <r.grannell2@gmail.com>
2013-11-09 19:59:50 +0000

    added results

--- inst/examples/example-xMap.R
+++ inst/examples/example-xMap.R
@@ -1,6 +1,14 @@
 
 # capitalise the lower-case letters.
+
 LETTERS <- xMap(toupper, letters)
+
+list(
+	"A", "B", "C", "D", "E", "F",
+	"G", "H", "I", "J", "K", "L",
+	"M", "N", "O", "P", "Q", "R",
+	"S", "T", "U", "V", "W", "X",
+	"Y", "Z")
 
 # grab the second value in a collection
 # of collections.
@@ -12,12 +20,11 @@ xMap(
 		list(1, 2, 3))
 )
 
+list("b", 2)
+
 # a mundane example; doubling
 # a vector of integers.
 
-xMap(
-	n := {
-		n + n
-	},
-	1:5
-)
+xMap(n := {n + n}, 1:5)
+
+list(2L, 4L, 6L, 8L, 10L)
