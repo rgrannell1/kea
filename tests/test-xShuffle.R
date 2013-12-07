5726c8453c68f873b67d0153e1db73a4d8b02d28
rgrannell1 <r.grannell2@gmail.com>
2013-12-07 02:21:22 +0000

    updated shuffle tests

--- tests/test-xShuffle.R
+++ tests/test-xShuffle.R
@@ -1,28 +1,31 @@
 
+forall <- arrow:::forall
+test_cases <- arrow:::test_cases
+
 message("xShuffle")
 
-forall(
-	"shuffling the empty collection returns the empty list",
-	list(coll = G$collection_zero),
-	xShuffle(coll) %equals% list()
-)
+	forall(
+		"shuffling the empty collection returns the empty list",
+		test_cases$collection_zero,
+		xShuffle(coll) %equals% list()
+	)
 
-forall(
-	"shuffling preserves length",
-	list(coll = G$collection()),
-	length(xShuffle(coll)) %equals% length(coll)
-)
+	forall(
+		"shuffling preserves length",
+		test_cases$collection,
+		length(xShuffle(coll)) %equals% length(coll)
+	)
 
-forall(
-	"shuffling returns a list",
-	list(coll = G$collection()),
-	is.list(xShuffle(coll))
-)
+	forall(
+		"shuffling returns a list",
+		test_cases$collection,
+		is.list(xShuffle(coll))
+	)
 
 message("arrow $ xShuffle")
 
-forall(
-	"shuffling preserves length",
-	list(coll = G$collection()),
-	length(x_(coll)$xShuffle()$x()) %equals% length(coll)
-)
+	forall(
+		"shuffling preserves length",
+		test_cases$collection,
+		length(x_(coll)$xShuffle()$x()) %equals% length(coll)
+	)
