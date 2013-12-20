feff4f724c0c14eb5db631297fa95877a023ca89
rgrannell1 <r.grannell2@gmail.com>
2013-12-20 16:58:25 +0000

    added monoidal tests for reduces

--- tests/test-xReducel.R
+++ tests/test-xReducel.R
@@ -2,5 +2,72 @@
 forall <- arrow:::forall
 test_cases <- arrow:::test_cases
 
-message("xReducel")
+message("xReduce")
 
+	forall(
+		"reducing the empty collection is the empty collection",
+		test_cases$collection_zero,
+		xReduce(stop, coll) %equals% coll
+	)
+
+	forall(
+		"reducing one value is that value",
+		test_cases$num_positive_integer,
+		xReduce(stop, num) %equals% num
+	)
+
+	forall(
+		"Return( ) terminates immediately",
+		test_cases$num_positive_integer,
+		xReduce(
+			function (acc, new) {
+
+				if (acc == 1) Return (acc) else c(acc, new)
+
+			},
+			seq_len(num)) == 1
+	)
+
+	forall(
+		"reducing plus is sum",
+		test_cases$sum_over_integers,
+		xReduce(fn, coll) %equals% sum(coll),
+		given =
+			length(coll) > 0
+	)
+
+message("arrow $ xReduce")
+
+	forall(
+		"collection $ xReduce",
+		test_cases$sum_over_integers,
+		x_(coll)$xReduce(fn)$x() %equals% sum(coll),
+		given =
+			length(coll) > 0
+	)
+
+	forall(
+		"collection $ xReduce",
+		test_cases$sum_over_integers,
+		x_(fn)$xReduce(coll)$x() %equals% sum(coll),
+		given =
+			length(coll) > 0
+	)
+
+message("arrow $ xReduce")
+
+	forall(
+		"collection $ x_Reduce",
+		test_cases$sum_over_integers,
+		x_(coll)$x_Reduce(fn) %equals% sum(coll),
+		given =
+			length(coll) > 0
+	)
+
+	forall(
+		"collection $ x_Reduce",
+		test_cases$sum_over_integers,
+		x_(fn)$x_Reduce(coll) %equals% sum(coll),
+		given =
+			length(coll) > 0
+	)
--- tests/test-xReducer.R
+++ tests/test-xReducer.R
@@ -4,3 +4,70 @@ test_cases <- arrow:::test_cases
 
 message("xReducer")
 
+	forall(
+		"reducing the empty collection is the empty collection",
+		test_cases$collection_zero,
+		xReducer(stop, coll) %equals% coll
+	)
+
+	forall(
+		"reducing one value is that value",
+		test_cases$num_positive_integer,
+		xReducer(stop, num) %equals% num
+	)
+
+	forall(
+		"Return( ) terminates immediately",
+		test_cases$num_positive_integer,
+		xReducer(
+			function (acc, new) {
+
+				if (acc == 1) Return (acc) else c(acc, new)
+
+			},
+			seq_len(num)) == 1
+	)
+
+	forall(
+		"reducing plus is sum",
+		test_cases$sum_over_integers,
+		xReducer(fn, coll) %equals% sum(coll),
+		given =
+			length(coll) > 0
+	)
+
+message("arrow $ xReducer")
+
+	forall(
+		"collection $ xReducer",
+		test_cases$sum_over_integers,
+		x_(coll)$xReducer(fn)$x() %equals% sum(coll),
+		given =
+			length(coll) > 0
+	)
+
+	forall(
+		"collection $ xReducer",
+		test_cases$sum_over_integers,
+		x_(fn)$xReducer(coll)$x() %equals% sum(coll),
+		given =
+			length(coll) > 0
+	)
+
+message("arrow $ xReducer")
+
+	forall(
+		"collection $ x_Reducer",
+		test_cases$sum_over_integers,
+		x_(coll)$x_Reducer(fn) %equals% sum(coll),
+		given =
+			length(coll) > 0
+	)
+
+	forall(
+		"collection $ x_Reducer",
+		test_cases$sum_over_integers,
+		x_(fn)$x_Reducer(coll) %equals% sum(coll),
+		given =
+			length(coll) > 0
+	)
