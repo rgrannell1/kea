ca0183842656263692bbceee9a8406f6da1cac75
rgrannell1 <r.grannell2@gmail.com>
2014-01-27 11:46:02 +0000

    added examples

--- inst/examples/example-xIterate.R
+++ inst/examples/example-xIterate.R
@@ -107,42 +107,14 @@ modulo(1121, 10)
 
 
 
-# 1. CE Capitalise the first letter of each word in a sentence.
+# 1. CE seperate a csv of mixed digits and letters
+#    In this case, pairs of strings and numbers.
 #
 
-text <-
-"Assyria was a major Semitic kingdom or empire of the Ancient Near East,
-existing in various forms during a period of approximately nineteen
-centuries from circa 2500 BC to 605 BC, spanning the Early Bronze Age
-through to the late Iron Age. Centered on the Upper Tigris river, in
-northern Mesopotamia(Iraq), the Assyrians came to rule powerful empires at
-several times. As substantial part of the greater Mesopotamian 'cradle of
-civilization', Assyria was at the height of technological, scientific and
-cultural achievements for its time. At its peak, the Assyrian empire
-stretched from the Mediterranean Sea to the Caspian Sea, and from the
-foothills of the Caucasus to Arabia."
 
-x_(text) $
-xToWords() $
-xMap(word := {
+csv <- "Monaco,48.9,Japan,44.6,Italy,44.3,Germany,43.7,Jersey,43.4"
 
-    chars <- xToChars(word)
-    if (chars[1] %in% letters) {
-        c(toupper(chars[1]), xRestOf(chars))
-    } else {
-        chars
-    }
-}) $
-xMap(xFromChars) $
-x_FromWords()
+x_(csv) $ xExplode(',') $ xGroup(2) $ x_Zip()
 
-# "Assyria Was A Major Semitic Kingdom Or Empire Of The Ancient Near East,
-# Existing In Various Forms During A Period Of Approximately Nineteen
-# Centuries From Circa 2500 BC To 605 BC, Spanning The Early Bronze Age
-# Through To The Late Iron Age. Centered On The Upper Tigris River, In
-# Northern Mesopotamia(Iraq), The Assyrians Came To Rule Powerful Empires At
-# Several Times. As Substantial Part Of The Greater Mesopotamian 'cradle Of
-# Civilization', Assyria Was At The Height Of Technological, Scientific
-# And Cultural Achievements For Its Time. At Its Peak, The Assyrian Empire
-# Stretched From The Mediterranean Sea To The Caspian Sea, And From The
-# Foothills Of The Caucasus To Arabia."
+
+
---
+++ inst/examples/example-xZip.R
@@ -0,0 +1,27 @@
+
+# seperate a csv of country,median age pairs
+# into a list of countries and ages.
+# this is a nicer format for Arrow to work with.
+
+csv <- "Monaco,48.9,Japan,44.6,Italy,44.3,Germany,43.7,Jersey,43.4"
+
+x_(csv) $ xExplode(',') $ xChunk(2) $ xZip() $
+x_AsNamed(c('country', 'age'))
+
+# list(
+#     country =
+#     list(
+#         "Monaco",
+#         "Japan",
+#         "Italy",
+#         "Germany",
+#         "Jersey"),
+#     age =
+#     list(
+#         "48.9",
+#         "44.6",
+#         "44.3",
+#         "43.7",
+#         "43.4"
+# ))
+
