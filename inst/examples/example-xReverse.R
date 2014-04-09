cb52413de3b63322d723728d7d44e31c43297f28
Ryan Grannell <r.grannell2@gmail.com>
2014-04-09 20:54:24 +0100

    added more examples

--- inst/examples/example-xReverse.R
+++ inst/examples/example-xReverse.R
@@ -1,3 +1,8 @@
 
-NULL
+# 1.
+# select the third last letter in the english alphabet.
 
+x_(letters) $ xReverse() $ xThirdOf()
+
+# the above is more semantic than
+# x_(letters) $ xAt(length(letters) - 3)
