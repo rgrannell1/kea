af88e2037858907ed2b2b194827aa9df8dfe09e8
Ryan Grannell <r.grannell2@gmail.com>
2014-04-12 22:11:05 +0100

    fixed scan test

--- tests/test-xScan.R
+++ tests/test-xScan.R
@@ -9,5 +9,11 @@ message("xScan")
 	forall(
 		"scan with the empty list is list of val.",
 		test_cases$num_integer,
-		xScan("+",0, list()) %equals% list(val)
+		xScan("+",0, list()) %equals% list(0)
+	)
+
+	forall(
+		"scan with the empty list is list of val.",
+		test_cases$num_positive_integer,
+		all.equal( xScan("+",0, 1:num), as.list(cumsum(0:num)) )
 	)
