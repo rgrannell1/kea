418cd39399a9c4aaef85618292dd4b160596aed9
Ryan Grannell <r.grannell2@gmail.com>
2014-04-13 03:37:46 +0100

    added xList example

--- inst/examples/example-xList.R
+++ inst/examples/example-xList.R
@@ -22,3 +22,43 @@ xList[paste(first_name, middleinit, sirname), middleinit <- c('K', 'M'), sirname
 # list("Jack K Black", "Jack M Black",
 #      "Jack K Brown", "Jack M Brown",
 #      "Jack K Gray", "Jack M Gray")
+
+
+# 3.
+# Using list comprehensions to process tabular data.
+
+raw_health_ranking <-
+"
+1     France             4
+2     Italy             11
+3     San-Marino        21
+4     Andorra           23
+5     Malta             37
+6     Singapore         38
+7     Spain             24
+8     Oman              62
+9     Austria            6
+10    Japan             13
+11    Norway            16
+12    Portugal          27
+13    Monaco            12
+14    Greece            30
+15    Iceland           14
+16    Luxembourg         5
+17    Netherlands        9
+18    United-Kingdom    26
+"
+
+health_ranking <-
+	x_(raw_health_ranking) $ xToLines() $ x_Map(xToWords)
+
+top_twenty_countries <-
+	xList[ tolower( row[[2]] ), row <- health_ranking ]
+
+# list(
+#	"france", "italy", "san-marino", "andorra",
+#	"malta", "singapore", "spain", "oman", "austria",
+#	"japan", "norway", "portugal", "monaco",
+#	"greece", "iceland", "luxembourg", "netherlands",
+#	"united-kingdom"
+# )
