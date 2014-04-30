b863c4e469769354881721f2b0ff39c170522896
Ryan Grannell <rgrannell1@users.noreply.github.com>
2014-04-30 22:51:19 +0100

    Update xChop.R

--- R/xChop.R
+++ R/xChop.R
@@ -1,24 +1,25 @@
-
 #' xChop
 #'
 #' Divide a collection into a fixed number of segments.
 #'
+#' @section Type Signature:
+#'     |numeric| -> |any| -> ||any||
+#'
 #' @details
+#'     \bold{xChop} is used to divide a collection into several
+#'     segments. This differs from \bold{xChunk} in that \bold{xChop}
+#'     creates a fixed number of segments or arbitrary size, while
+#'     \bold{xChunk} creates segments of fixed size, but not a fixed
+#'     number of such segments.
 #'
-#' \bold{xChop} is used to divide a collection into several
-#' segments. This differs from \bold{xChunk} in that \bold{xChop}
-#' creates a fixed number of segments or arbitrary size, while
-#' \bold{xChunk} creates segments of fixed size, but not a fixed
-#' number of such segments.
+#'     \code{xChop(2, 1:5)}
 #'
-#' \code{xChop(2, 1:5)}
+#'     \code{list(list(1, 2, 3), list(4, 5))}
 #'
-#' \code{list(list(1, 2, 3), list(4, 5))}
-#'
-#' \bold{xChop} can be used for dividing a task for parallel execution.
-#' For example, a very large collection could be chopped into
-#' four subcollections by using \bold{xChop(4, coll)}, before applying a
-#' function to each sublist using \bold{mclapply( )}.
+#'     \bold{xChop} can be used for dividing a task for parallel execution.
+#'     For example, a very large collection could be chopped into
+#'     four subcollections by using \bold{xChop(4, coll)}, before applying a
+#'     function to each sublist using \bold{mclapply( )}.
 #'
 #' @param
 #'    num a nonnegative whole number. The desired number
