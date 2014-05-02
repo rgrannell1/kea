22e43de3444296dbfc18eec8f071f871e5b6eba3
Ryan Grannell <r.grannell2@gmail.com>
2014-05-03 00:54:41 +0100

    added warning for missing type signatures

--- tests/test-x_Files.R
+++ tests/test-x_Files.R
@@ -83,3 +83,29 @@ message('check that there are sufficient examples')
 
 		invisible(Null)
 	})
+
+
+
+
+
+
+
+
+
+message('check that there is a type signature for every file')
+
+	missing_types <-
+		x_(list.files('/home/ryan/Code/arrow.R/R', full.names = True)) $
+		xSelect(path := grepl('x[A-Z].+', path)) $
+		xMap(path := c(path, path)) $ xZipKeys() $
+		xMap(xReadLines) $ xMap(file := {
+
+			x_(file) $ x_Any(line := grepl("Type Signature", line))
+
+		}) $
+		# -- get the paths that don't have examples and should.
+		xReject(xI) $ xUnzipKeys() $ xMap(xFirstOf) $ xMap( path := xLastOf(xExplode('/', path)) )
+
+	if (!missing_types $ x_IsSubset_("xLambda.R", "xList.R", "")) {
+		warning("type signatures missing from ", missing_types $ x_Implode(", "))
+	}
