51bd918d02eed304bb94849b38657633874dde0b
Ryan Grannell <r.grannell2@gmail.com>
2014-05-09 13:46:36 +0100

    added better test for xUnzipIndices

--- tests/test-xUnzipIndices.R
+++ tests/test-xUnzipIndices.R
@@ -11,3 +11,23 @@ message("xUnzipIndices")
 		test_cases$collection_zero,
 		xUnzipIndices(coll) %equals% list()
 	)
+
+	forall(
+		"unzipping indiced is length two",
+		test_cases$collection,
+		all(sapply( xUnzipIndices(coll), length ) == 2)
+	)
+
+	forall(
+		"the first column is the indices",
+		test_cases$collection,
+		all(sapply( xUnzipIndices(coll), function (x) x[[1]] ) == seq_along(coll))
+	)
+
+	forall(
+		"the second column is the values",
+		test_cases$collection,
+		all( sapply(seq_along(coll), function (ith) {
+			identical(coll[[ith]], xUnzipIndices(coll)[[ith]][[2]])
+		}) )
+	)
