e1c650e4b41f76355a0c01cdd46399a12adf6eaa
rgrannell1 <r.grannell2@gmail.com>
2014-06-05 07:29:58 +0100

    removed old tests from xPoll

--- tests/test-xPoll.R
+++ tests/test-xPoll.R
@@ -1,47 +1,4 @@
 
-forall <- arrow:::forall
-test_cases <- arrow:::test_cases
-
-require(arrow)
-
-message("xPoll")
-
-	forall(
-		"polling the empty collection returns integer(0)",
-		test_cases$collection_zero,
-		xPoll(Truth, coll) %equals% integer(0))
-
-	forall(
-		"polling with truth returns length coll",
-		test_cases$collection,
-		xPoll(Truth, coll) == length(coll),
-		given =
-			length(coll) > 0)
-
-	forall(
-		"polling with falsity returns 0",
-		test_cases$collection,
-		xPoll(Falsity, coll) == 0,
-		given =
-			length(coll) > 0)
-
-	forall(
-		"polling with moot returns 0",
-		test_cases$collection,
-		xPoll(Moot, coll) == 0,
-		given =
-			length(coll) > 0)
-
-	forall(
-		"polling counts true occurrences",
-		test_cases$mod2_over_ints,
-		xPoll(fn, coll) == length(which(coll %% 2 == 0)),
-		given =
-			length(coll) > 0)
-
-
-
-
 arrow ::: load_test_dependencies(environment())
 is_collection <- arrow ::: is_collection
 
