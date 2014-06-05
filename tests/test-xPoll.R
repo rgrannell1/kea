1c31d59323e4cfe5a3b31b3d0c28099244a35cf1
rgrannell1 <r.grannell2@gmail.com>
2014-06-05 07:27:33 +0100

    added tests for xPoll

--- tests/test-xPoll.R
+++ tests/test-xPoll.R
@@ -38,3 +38,41 @@ message("xPoll")
 		xPoll(fn, coll) == length(which(coll %% 2 == 0)),
 		given =
 			length(coll) > 0)
+
+
+
+
+arrow ::: load_test_dependencies(environment())
+is_collection <- arrow ::: is_collection
+
+message("xPoll (+)")
+
+	over(coll) +
+	describe('xPoll counts true occurrences.') +
+	when(
+		is.logical(coll) && length(coll) > 0,
+		xPoll(identity, coll) %equals% length(which(coll)) == 0
+	) +
+
+	describe('xPoll with the empty collection is integer(0)') +
+	when(
+		is_collection(coll) && length(coll) == 0,
+		xPoll(function (x) True, coll)  %equals% integer(0),
+		xPoll(function (x) False, coll) %equals% integer(0),
+		xPoll(function (x) Na, coll)    %equals% integer(0)
+	) +
+
+	describe('xPoll with xFalsity is 0') +
+	when(
+		is_collection(coll) && length(coll) > 0,
+		xPoll(function (x) False, coll) == 0,
+		xPoll(function (x) Na, coll)    == 0
+	) +
+
+	describe('xPoll with xTruth is length') +
+	when(
+		is_collection(coll) && length(coll) > 0,
+		xPoll(function (x) True, coll) == length(coll)
+	) +
+
+	run()
