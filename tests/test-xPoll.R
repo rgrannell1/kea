17f282196ae6b3eeb0ff3f242b34b9e1d68f651e
rgrannell1 <r.grannell2@gmail.com>
2014-06-05 11:18:20 +0100

    bumped version

--- tests/test-xPoll.R
+++ tests/test-xPoll.R
@@ -15,9 +15,9 @@ message("xPoll (+)")
 	describe('xPoll with the empty collection is integer(0)') +
 	when(
 		is_collection(coll) && length(coll) == 0,
-		xPoll(function (x) True, coll)  %equals% integer(0),
+		xPoll(function (x) True,  coll) %equals% integer(0),
 		xPoll(function (x) False, coll) %equals% integer(0),
-		xPoll(function (x) Na, coll)    %equals% integer(0)
+		xPoll(function (x) Na,    coll) %equals% integer(0)
 	) +
 
 	describe('xPoll with xFalsity is 0') +
