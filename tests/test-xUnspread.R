3175a5d1b2031cc6609297cc44fa56e1ff706521
rgrannell1 <r.grannell2@gmail.com>
2014-06-08 05:29:23 +0100

    added test for xUnspread

--- tests/test-xUnspread.R
+++ tests/test-xUnspread.R
@@ -1,19 +1,16 @@
 
-forall <- kiwi:::forall
-test_cases <- kiwi:::test_cases
+kiwi ::: load_test_dependencies(environment())
+is_collection <- kiwi ::: is_collection
 
-require(kiwi)
+message("xUnspread (+)")
 
-message("xUnspread")
+	over(fn) +
 
-	forall(
-		"all base functions become unary with xUnspread",
-		test_cases$base_function,
-			length( formals(xUnspread(fn) )) == 1
-	)
+	describe('all functions become unary') +
+	when(
+		is.function(fn),
+		length( formals(xUnspread(fn) )) == 1
+	) +
 
-	forall(
-		"xUnspread of plus works properly",
-		test_cases$sum_over_integers,
-		all(xUnspread(fn)( list(coll, coll) ) == coll + coll)
-	)
+	run()
+
