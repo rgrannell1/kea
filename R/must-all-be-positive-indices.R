c69a44c46902acece1ba204d19f3f9762ef0feeb
rgrannell1 <r.grannell2@gmail.com>
2014-06-16 15:27:30 +0100

    improved message for positive indices macro

--- R/must-all-be-positive-indices.R
+++ R/must-all-be-positive-indices.R
@@ -9,18 +9,25 @@ Must_All_Be_Positive_Indices <- function (NUMS, COLL) {
 	NUMS <- substitute(NUMS)
 	COLL <- substitute(COLL)
 
-	bquote( if (is.infinite( .(NUMS) )) {
+	bquote( if ( any(is.infinite( .(NUMS) )) ) {
 
 		message <-
-			""
-		message <-
 			"The argument matching " %+% ddquote( .(NUMS) ) %+%
-			" must be positive indices of the collection matching " %+% ddquote( .(COLL) ) %+% "." %+%
+			"must be positive indices of the collection matching " %+% ddquote( .(COLL) ) %+%
+			".\n\n" %+%
+			"The actual argument contained infinite values." %+%
 			summate( .(NUMS) )
 
 		throw_kiwi_error(sys.call(), message)
 
 	} else if (any( .(NUMS) > length( .(COLL) ) | .(NUMS) < 1 )) {
+
+		message <-
+			"The argument matching " %+% ddquote( .(NUMS) ) %+%
+			"must be positive indices of the collection matching " %+% ddquote( .(COLL) ) %+% "." %+%
+			summate( .(NUMS) )
+
+		throw_kiwi_error(sys.call(), message)
 
 	} else {
 		TRUE
