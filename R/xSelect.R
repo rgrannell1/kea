84ac5ac8c60b01aca404d0067877446eb3ac6cab
rgrannell1 <r.grannell2@gmail.com>
2014-06-18 00:38:33 +0100

    added more flags to predicates.

--- R/xSelect.R
+++ R/xSelect.R
@@ -50,7 +50,12 @@ xSelect <- MakeFun(function (pred, coll) {
 	} else {
 
 		ind <- vapply(coll, function (elem) {
-			isTRUE(pred(elem))
+
+			is_match <- pred(elem)
+			Must_Be_Flag(is_match, pred)
+
+			isTRUE(is_match)
+
 		}, logical(1), USE.NAMES = False)
 
 		as.list( coll[ !elem_is_na(ind) & ind ] )
