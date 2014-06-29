367c2157ae99e8023a02bfdc5aad1f1a1852a93e
rgrannell1 <r.grannell2@gmail.com>
2014-06-29 03:33:52 +0100

    added missing precondition to drop tests

--- tests/test-xDrop.R
+++ tests/test-xDrop.R
@@ -9,7 +9,7 @@ message('xDrop')
 	describe("dropping from the empty collection is the empty collection") +
 	holdsWhen(
 		is.numeric(num) && !is.na(num) &&
-		(length(num) == 0 || (num > 0 && round(num) == num)) &&
+		(length(num) == 0 || (length(num) == 1 && num > 0 && round(num) == num)) &&
 		is_collection(coll) && length(coll) == 0,
 		xDrop(num, coll) %is% list()
 	) +
@@ -17,7 +17,7 @@ message('xDrop')
 	describe("dropping yields the correct collection") +
 	holdsWhen(
 		is.numeric(num) && !is.na(num) &&
-		(length(num) == 0 || (num > 0 && round(num) == num)) &&
+		(length(num) == 0 || (length(num) == 1 && num > 0 && round(num) == num)) &&
 		is_collection(coll) && length(coll) > 0,
 		{
 			ind <- min(length(coll), num)
@@ -28,7 +28,7 @@ message('xDrop')
 	describe("take works over all round positive integers") +
 	worksWhen(
 		is.numeric(num) && !is.na(num) &&
-		(length(num) == 0 || (num > 0 && round(num) == num)) &&
+		(length(num) == 0 || (length(num) == 1 && num > 0 && round(num) == num)) &&
 		is_collection(coll),
 		xDrop(num, coll)
 	) +
