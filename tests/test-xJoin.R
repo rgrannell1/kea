c29fa47c395adc12a3cfbb5c206e0de621efe315
rgrannell1 <r.grannell2@gmail.com>
2014-06-30 01:28:23 +0100

    updated join test

--- tests/test-xJoin.R
+++ tests/test-xJoin.R
@@ -4,17 +4,18 @@ kiwi ::: load_test_dependencies(environment())
 
 message("xJoin (+)")
 
-	over(coll1, coll2, coll3) +
+	over(coll1, coll2) +
 
 	describe('a single collection acts as identity') +
 	holdsWhen(
-		is_collection(coll1),
+		is_collection(coll1) && length(coll1) > 0,
 		xJoin(list(coll1)) %is% as.list(coll1)
 	) +
 
 	describe('joining an empty collection with non-empty is the non-empty collection (left)') +
 	holdsWhen(
-		is_collection(coll1) && is_collection(coll2) && length(coll2) == 0,
+		is_collection(coll1) &&
+		is_collection(coll2) && length(coll2) == 0 && !is_named(coll2),
 		xJoin(list(coll1, coll2)) %is% as.list(coll1),
 		xJoin(list(coll2, coll1)) %is% as.list(coll1)
 	) +
