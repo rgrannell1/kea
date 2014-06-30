70e047632b18626b82ed5fdb37d9d5e6615dc091
rgrannell1 <r.grannell2@gmail.com>
2014-06-30 01:07:01 +0100

    removed old flatmap tests.

--- tests/test-xFlatMap.R
+++ tests/test-xFlatMap.R
@@ -1,26 +1,5 @@
 
-forall <- kiwi:::forall
-test_cases <- kiwi:::test_cases
-
-require(kiwi)
-
-message("xFlatMap")
-
-	forall(
-		"flatmapping increment increments the list",
-		test_cases$succ_over_integers,
-		all( unlist(xFlatMap(fn, coll)) == unlist(coll) + 1 )
-	)
-
-	forall(
-		"flatmapping can extend a collection length.",
-		test_cases$integers,
-		length(xFlatMap(function (x) c(x, x), coll)) == 2 * length(coll)
-	)
-
-
 kiwi ::: load_test_dependencies(environment())
-
 
 message("xFlatMap")
 
