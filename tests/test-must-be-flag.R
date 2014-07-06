b4374750fae4d3706a64e9bbea42a4fa9cb7cfc1
rgrannell1 <r.grannell2@gmail.com>
2014-07-06 22:34:29 +0100

    added test for flag macro

--- tests/test-must-be-flag.R
+++ tests/test-must-be-flag.R
@@ -1,2 +1,28 @@
 
-NULL
+kiwi ::: load_test_dependencies(environment())
+
+to_test       <- function (bool) {}
+body(to_test) <- kiwi ::: Must_Be_Flag(bool, predSymbol)
+
+message('Must_Be_Flag')
+
+	over(bool) +
+
+	describe('works for true, false, na') +
+	worksWhen(
+		is.logical(bool) && length(bool) == 1,
+		to_test(bool)
+	) +
+
+	run()
+
+	over(bool) +
+
+	describe('fails for anything else.') +
+	failsWhen(
+		!is.logical(bool) || length(bool) != 1,
+		to_test(bool)
+	) +
+
+	run()
+
