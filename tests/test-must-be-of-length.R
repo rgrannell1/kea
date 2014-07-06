b23621075497dcf7183ae006d90372b8d4a0a8a6
rgrannell1 <r.grannell2@gmail.com>
2014-07-06 22:43:02 +0100

    added test for must be of length

--- tests/test-must-be-of-length.R
+++ tests/test-must-be-of-length.R
@@ -1,2 +1,33 @@
 
-NULL
+kiwi ::: load_test_dependencies(environment())
+
+to_test       <- function (coll, lengths) {}
+body(to_test) <- kiwi ::: Must_Be_Of_Length(coll, lengths)
+
+message('Must_Be_Of_Length')
+
+	over(coll) +
+
+	describe("must have length is true for collection length") +
+	worksWhen(
+		is_collection(coll),
+		to_test(coll, length(coll))
+	) +
+
+	describe("works when one length is correct") +
+	worksWhen(
+		is_collection(coll),
+		to_test(coll, c(length(coll) - 1, length(coll), length(coll) + 1))
+	) +
+
+	run()
+
+	over(coll, nums) +
+
+	describe('fails when length does not match') +
+	failsWhen(
+		is_collection(coll) && is.numeric(nums) && !( any(nums == length(coll) )),
+		to_test(coll, nums)
+	) +
+
+	run()
