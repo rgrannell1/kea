ba1058801f06c37b96326364700a48e8717fc1e5
rgrannell1 <r.grannell2@gmail.com>
2014-07-06 22:19:52 +0100

    added macro tests.

--- tests/test-must-all-be-indices.R
+++ tests/test-must-all-be-indices.R
@@ -1,5 +1,27 @@
 
 kiwi ::: load_test_dependencies(environment())
 
+to_test       <- function (nums, coll) {}
+body(to_test) <- kiwi ::: Must_All_Be_Indices(nums, coll)
+
 message("Must_All_Be_Indices")
 
+	over(coll) +
+
+	describe('indices are always indices of coll') +
+	worksWhen(
+		is_collection(coll),
+		to_test(seq_along(coll), coll)
+	) +
+
+	run()
+
+	over(nums, coll) +
+
+	describe('always fails for infinites') +
+	failsWhen(
+		any(is.infinite(nums)) && is_collection(coll),
+		to_test(nums, coll)
+	) +
+
+	run()
