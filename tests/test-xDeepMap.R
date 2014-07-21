8b29c0ed6ef895d5b5b49d6918acd11b99a89ea8
rgrannell1 <r.grannell2@gmail.com>
2014-07-21 20:16:23 +0100

    updated xDeepMap test.

--- tests/test-xDeepMap.R
+++ tests/test-xDeepMap.R
@@ -8,7 +8,7 @@ message("xDeepMap")
 	describe('deepmapping over empty list is empty list') +
 	holdsWhen(
 		is_collection(coll) && length(coll) == 0,
-		xDeepMap(identity, coll) %is% list()
+		xDeepMap(identity, coll) %is% keep_names(list(), coll)
 	) +
 
 	run()
