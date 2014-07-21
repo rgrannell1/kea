f97ab690aa0643917e5263e26e7a555fa1806703
rgrannell1 <r.grannell2@gmail.com>
2014-07-21 16:26:50 +0100

    added tests for xTo_

--- tests/test-xToLines.R
+++ tests/test-xToLines.R
@@ -1,25 +1,14 @@
 
-forall <- kiwi:::forall
-test_cases <- kiwi:::test_cases
-
-require(kiwi)
+kiwi ::: load_test_dependencies(environment())
 
 message('xToLines')
 
-	forall(
-		"lines of the empty string is an empty vector",
-		list(),
+	over(strs) +
+
+	describe("works of empty string is empty vector") +
+	holdsWhen(
+		True,
 		xToLines('') %is% character(0)
-	)
+	) +
 
-	forall(
-		"xToLines of letters with newlines is pasted letters",
-		test_cases$letters_with_newlines,
-		{
-			delim <- paste0(str, collapse = '')
-			xToLines(paste0(coll, collapse = delim)) %is% coll
-		},
-		given =
-			length(coll) > 0 && length(str) > 0
-	)
-
+	run()
--- tests/test-xToWords.R
+++ tests/test-xToWords.R
@@ -1,25 +1,14 @@
 
-forall <- kiwi:::forall
-test_cases <- kiwi:::test_cases
-
-require(kiwi)
+kiwi ::: load_test_dependencies(environment())
 
 message('xToWords')
 
-	forall(
-		"words of the empty string is an empty vector",
-		list(),
+	over(strs) +
+
+	describe("works of empty string is empty vector") +
+	holdsWhen(
+		True,
 		xToWords('') %is% character(0)
-	)
+	) +
 
-	forall(
-		"xToWords of letters with space is pasted letters",
-		test_cases$letters_with_spaces,
-		{
-			delim <- paste0(str, collapse = '')
-			xToWords(paste0(coll, collapse = delim)) %is% coll
-		},
-		given =
-			length(coll) > 0 && length(str) > 0
-	)
-
+	run()
