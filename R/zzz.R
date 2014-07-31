f7eba22711ed4b1c6461442e16f1480e85a9737b
rgrannell1 <r.grannell2@gmail.com>
2014-07-31 21:45:07 +0100

    added startup messages.

---
+++ R/zzz.R
@@ -0,0 +1,16 @@
+
+# Adapted from ggplot2's helpful messages.
+
+.onAttach <- function (...) {
+
+	if (!interactive() || runif(1) > 0.1) {
+		return()
+	}
+
+	messages <- c(
+		"Please submit bug reports and feature requests to: https://github.com/rgrannell1/kiwi/issues."
+	)
+
+	message <- sample(messages, 1)
+	packageStartupMessage(message)
+}
