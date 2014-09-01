a4ac057f64458c83eec6ee2f39ebf685fe8a7249
rgrannell1 <r.grannell2@gmail.com>
2014-09-01 15:51:10 +0100

    fixed bad reference in test

--- tests/test-xNotPrefixOf.R
+++ tests/test-xNotPrefixOf.R
@@ -1,7 +1,7 @@
 
 kea ::: load_test_dependencies(environment())
 
-message("xNotPrefix")
+message("xNotPrefixOf")
 
 	over(coll1, coll2) +
 
@@ -10,21 +10,21 @@ message("xNotPrefix")
 		is_collection(coll1) && is_collection(coll2) &&
 		(length(coll1) == 0 || length(coll2) == 0),
 
-		xNotPrefix(coll1, coll2) %is% logical(0)
+		xNotPrefixOf(coll1, coll2) %is% logical(0)
 	) +
 
 	describe("a collection is a prefix of itself.") +
 	holdsWhen(
 		is_collection(coll1) && length(coll1) > 0,
 
-		!xNotPrefix(coll1, coll1)
+		!xNotPrefixOf(coll1, coll1)
 	) +
 
 	describe("continuous subsequences are always members.") +
 	holdsWhen(
 		is_collection(coll1) && length(coll1) > 0,
 
-		!xNotPrefix(
+		!xNotPrefixOf(
 			head(coll1, max(c(1, sample.int(length(coll1), 1)) )),
 			coll1)
 	) +
