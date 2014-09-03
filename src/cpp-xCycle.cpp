a5b16d759b00f87585b4fcb833e4214c85e6d290
rgrannell1 <r.grannell2@gmail.com>
2014-09-03 17:22:13 +0100

    fixed a crashing error.

--- src/cpp-xCycle.cpp
+++ src/cpp-xCycle.cpp
@@ -1,4 +1,5 @@
 #include <Rcpp.h>
+#include <stdlib.h>
 using namespace Rcpp;
 
 // [[Rcpp::export]]
@@ -10,31 +11,30 @@ List cCycle (NumericVector num, List coll) {
 	int num_mag  = num[0];
 
 	bool has_names             = coll.attr("names") != R_NilValue;
-	/* needed to avoid a type-error when unnamed. */
-	CharacterVector coll_names = coll.attr("names") != R_NilValue ?
-		coll.attr("names"):
-		CharacterVector::create();
+	CharacterVector coll_names = coll.attr("names") != R_NilValue ? coll.attr("names"): CharacterVector::create();
 
 	if (coll_len == 0 || num_len == 0) {
+
+		return List::create();
 
 	} else {
 
 		List out(coll_len);
-		List coll_names(coll_len);
+		List out_names(coll_len);
 
 		for (int ith = 0; ith < coll_len; ith++) {
 
-			int index = (num_mag + ith) % coll_len;
+			int index = abs((num_mag + ith)) % coll_len;
 			out[ith]  = coll[index];
 
 			if (has_names) {
-				coll_names[ith] = coll_names[index];
+				out_names[ith] = coll_names[index];
 			}
 
 		}
 
 		if (has_names) {
-			out.attr("names") = coll_names;
+			out.attr("names") = out_names;
 		}
 
 		return out;
