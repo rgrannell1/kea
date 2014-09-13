32a0a3d831e4245511b9b0066fdcdbdcdd63b488
rgrannell1 <r.grannell2@gmail.com>
2014-09-13 21:01:53 +0100

    added xCharsO

---
+++ R/xCharsOf.R
@@ -0,0 +1,29 @@
+
+#' xCharsOf
+#'
+#' Get the number of characters in a string.
+#'
+#' @section Type Signature:
+#'     |character| -> &lt;character>
+#'
+#' @param
+#'     str a length-one character vector. The string to
+#'     get the numbers of characters in.
+#'
+#' @return
+#'     An integer.
+#'
+#' @section Corner Cases:
+#'    none.
+#'
+#' @family text_processing_functions
+#'
+#' @example
+#'    inst/examples/example-xCharsOf.R
+#'
+#' @rdname xCharsOf
+#' @export
+
+xCharsOf <- MakeFun('xCharsOf', function (str) {
+	nchar(str)
+})
---
+++ tests/test-xCharsOf.R
@@ -0,0 +1,23 @@
+
+kea ::: load_test_dependencies(environment())
+
+message("xCharsOf")
+
+	over(str) +
+
+	describe("charsof is an integer") +
+	holdsWhen(
+		is_character(str) && length(str) == 1 && !is.na(str),
+
+		is.integer(xCharsOf(str)),
+		length(xCharsOf) == 1
+	) +
+
+	describe("charsof is an integer") +
+	holdsWhen(
+		is_character(str) && length(str) == 1 && !is.na(str),
+
+		xCharsOf(str) %is% nchar(unlist(str))
+	) +
+
+	run()
