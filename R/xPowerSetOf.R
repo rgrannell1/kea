d5f99c73736241270f716057d60b4000bb71fc0b
rgrannell1 <r.grannell2@gmail.com>
2014-09-19 02:13:48 +0100

    improved efficiency of xPowerSetOf using std::vector

--- R/xPowerSetOf.R
+++ R/xPowerSetOf.R
@@ -37,32 +37,6 @@
 #' @export
 
 xPowerSetOf <- MakeFun('xPowerSetOf', function (coll) {
-
-#	if (length(coll) == 0) {
-#		list()
-#	} else {
-#
-#		subsets <- list(list())
-#		coll <- lapply(coll, list)
-#
-#		for (elem in coll) {
-#
-#			elem_subsets <- vector('list', length(subsets))
-#
-#			# -- append the element to each set in the partial
-#			# -- powerset, until all elements are added. Tree-like branching.
-#
-#			for (ith in seq_along(subsets)) {
-#				elem_subsets[[ith]] <- c(subsets[[ith]], elem)
-#			}
-#
-#			subsets <- c(subsets, elem_subsets)
-#
-#		}
-#
-#		subsets
-#	}
-
 	cPowerSetOf(coll)
 })
 
