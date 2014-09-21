e0fc554352154b10bf840149b55c5206cd30c4ff
rgrannell1 <r.grannell2@gmail.com>
2014-09-21 23:58:13 +0100

    updated news

---
+++ inst/examples/example-xDistinctOf.R
@@ -0,0 +1,27 @@
+
+# 1. Find the smallest number in a list.
+
+x__(1, 2, 3, 4, 5) $ x_MinBy(xI)
+
+# 1
+
+# 2. CodeEvalis a string a pangram?
+#    does a string contain each letter.
+
+is_pangram <- line := {
+
+	x_(line) $
+	xToChars() $ xMap(tolower) $ xDistinctOf() $
+	xSelect(
+		xIsIn(coll = letters)
+	) $
+	x_LenOf() == 26
+}
+
+is_pangram('A quick brown fox jumps over the lazy dog')
+
+# True
+
+is_pangram('not pangramic thank you mam.')
+
+# False
