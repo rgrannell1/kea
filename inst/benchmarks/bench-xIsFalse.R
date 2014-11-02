516e9bb3507e0c3173b3d972946104d896aab089
rgrannell1 <r.grannell2@gmail.com>
2014-11-02 18:37:01 +0000

    fixed bad reference

---
+++ inst/benchmarks/bench-xIsFalse.R
@@ -0,0 +1,4 @@
+list()
+
+xIsFalse(FALSE)
+xIsFalse(TRUE)
---
+++ inst/benchmarks/bench-xThirdOf.R
@@ -0,0 +1,9 @@
+list(
+	one_hundred  = 1:100,
+	one_thousand = 1:1000,
+	ten_thousand = 1:10000
+)
+
+xThirdOf(one_hundred)
+xThirdOf(one_thousand)
+xThirdOf(ten_thousand)
