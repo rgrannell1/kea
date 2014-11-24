f3b22d57a77aaeec5b073afc40a512ac5bb72bac
rgrannell1 <r.grannell2@gmail.com>
2014-11-24 16:04:25 +0000

    updated intersect tests

--- tests/test-xIntersect.R
+++ tests/test-xIntersect.R
@@ -9,21 +9,21 @@ message("xIntersect")
 	holdsWhen(
 		suchThat $ is_empty_collection(coll1) && suchThat $ is_collection(coll2) ,
 
-		xIntersect(coll1, coll2) %is% list()
+		xIntersect(coll1, coll2) %is% keep_names(list(), coll2)
 	) +
 
 	describe("interof of empty collection is empty (right)") +
 	holdsWhen(
 		suchThat $ is_collection(coll1) && suchThat $ is_empty_collection(coll2) ,
 
-		xIntersect(coll1, coll2) %is% list()
+		xIntersect(coll1, coll2) %is% keep_names(list(), coll2)
 	) +
 
 	describe("interof of empty collection is empty (both)") +
 	holdsWhen(
 		suchThat $ is_empty_collection(coll1) && suchThat $ is_empty_collection(coll2) ,
 
-		xIntersect(coll1, coll2) %is% list()
+		xIntersect(coll1, coll2) %is% keep_names(list(), coll2)
 	) +
 
 	describe("interof with self is self") +
