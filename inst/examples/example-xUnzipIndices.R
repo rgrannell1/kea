72c2b72b41547fe0354cbc5955c9382b6452d646
rgrannell1 <r.grannell2@gmail.com>
2014-11-24 02:47:15 +0000

    removed unspread

--- inst/examples/example-xUnzipIndices.R
+++ inst/examples/example-xUnzipIndices.R
@@ -1,6 +1,6 @@
 
 # 1. enumerate the letters with their indices.
 
-x_(letters) $ xUnzipIndices() $ xMap( xUnspread((ith : val) := {
+x_(letters) $ xUnzipIndices() $ xMap( xApply((ith : val) := {
 	xFromWords_('the ', paste(ith), ' letter is ', val)
 }) )
