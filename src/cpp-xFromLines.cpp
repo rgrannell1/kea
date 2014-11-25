4ef5ad0b321fb1b53f509874abcdde3029d9a47b
rgrannell1 <r.grannell2@gmail.com>
2014-11-25 16:46:32 +0000

    removed xAsDataframe

---
+++ src/cpp-xFromLines.cpp
@@ -0,0 +1,23 @@
+/*
+
+#include <Rcpp.h>
+#import <math.h>
+#include "functions.h"
+using namespace Rcpp;
+
+
+
+
+// [[Rcpp::export]]
+CharacterVector cFromLines (const std::string str) {
+
+	std::string out;
+
+	for (size_t ith = 0; ith < str.size(); ++ith) {
+		if (str[ith] != ' ') out += str[ith];
+	}
+
+	return out;
+
+}
+*/
