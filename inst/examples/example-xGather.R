ba5d35a9e88d3f440759faa30afe85725c52955d
rgrannell1 <r.grannell2@gmail.com>
2014-11-28 20:14:19 +0000

    added example to gather

--- inst/examples/example-xGather.R
+++ inst/examples/example-xGather.R
@@ -4,3 +4,15 @@
 xGather(c(0, 0, 0, 1, 0, 1, 1, 0))
 
 # xGather_(0, 0, 1, 1, 1, 0, 1, 0)
+
+
+
+
+# 2. Get the length of the longest stretch of consecutive one's in a binary
+#    vector (an interview question my friend was asked).
+
+bits <- c(0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0)
+
+x_(bits) $ xGather() $ xMap(xLenOf) $ x_MaxBy(xI)
+
+# 5
