9488e266034c2d0f9e54d81515c3f055fe94feff
rgrannell1 <r.grannell2@gmail.com>
2014-12-02 04:42:00 +0000

    updated error stringifying

--- R/tools-exceptions.R
+++ R/tools-exceptions.R
@@ -1,4 +1,69 @@
 
+
+
+
+
+# truncate
+#
+# string -> number -> string
+#
+# cut a string to a fixed length, and indicate that the string was truncated.
+
+truncate <- function (str, num) {
+
+	if (nchar(str) > num) {
+		paste0(substring(str, 1, num), '[truncated]', collapse = '')
+	} else {
+		str
+	}
+
+}
+
+
+
+
+
+stringify_call <- local({
+
+	# expand_call
+	#
+	# call -> call
+	#
+	# expand calls to := to their evaluated functions.
+	# refactor `:=`(a, {a + a}) to function (a) {a + a}
+
+	expand_call <- function (call) {
+
+		is_lambda <- function (term) {
+			is.call(term) && term[[1]] == as.symbol(':=')
+		}
+
+		as.call( lapply(call, function (term) {
+			if (is_lambda(term)) eval(term) else term
+		}) )
+
+	}
+
+
+
+
+
+	function (call) {
+
+		if (length(call) == 0) {
+			"[erroneous call not included]"
+		} else {
+			truncate( ddparse(expand_call(call)) , 50)
+		}
+
+	}
+
+})
+
+
+
+
+
 # kea_condition_message
 #
 # R prints the $ message field of its error objects. Kea's messages
@@ -8,9 +73,56 @@
 # (ecall, message) -> kea_condition_message -> $ message -> stop|warning
 #
 
-kea_condition_message <- function (ecall, message) {
+kea_condition_message <- local({
 
-}
+	stringify_call_fn <- function (ecall) {
+
+		calling <- ecall[[1]]
+
+		if (is.name(calling)) {
+			paste0( calling )
+		} else {
+			truncate(ddparse( calling ), 50)
+		}
+
+	}
+
+	call_parts <- function (ecall) {
+
+		list(
+			fn    = stringify_call_fn(ecall),
+			whole = stringify_call(ecall)
+		)
+
+	}
+
+
+
+
+
+	function (ecall, message) {
+
+		if (length(message) != 1) {
+
+			stop('internal error in kea; a non-length one error message was produced.' %+%
+				' Please report this at https://github.com/rgrannell1/kea/issues')
+
+		} else {
+
+			parts <- call_parts(ecall)
+
+			"\n" %+% message %+%
+			"\nThrown from " %+% parts $ fn %+%
+			"\nIn the call " %+% parts $ whole -> unpadded_message
+
+			gsub('\n', '\n ', unpadded_message)
+
+		}
+
+	}
+
+})
+
 
 
 
@@ -32,7 +144,7 @@ KeaCondition <- function (...) {
 		# -- the erroneous call. Represented as is. Not printed; for
 		# -- programmatic use up the call stack.
 
-		fields $ call    <- ecall
+		fields $ ecall    <- ecall
 
 		# -- what the user sees. Highly processed error message.
 
@@ -101,12 +213,18 @@ keaThrower <- function (constructor) {
 		cond <- constructor(invoking_call, message)
 
 		base_thrower <- if (inherits(cond, 'warning')) {
+
+			cond $ message <- colourise $ yellow(cond $ message)
 			warning
+
 		} else if (inherits(cond, 'error')) {
+
+			cond $ message <- colourise $ red(cond $ message)
 			stop
+
 		}
 
-		base_thrower(cond, call. = False)
+		base_thrower(cond)
 
 	}
 }
