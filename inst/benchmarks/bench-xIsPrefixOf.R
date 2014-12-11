2e733fee175b279468ee069b4513359d51e0fade
rgrannell1 <r.grannell2@gmail.com>
2014-12-11 05:17:47 +0000

    updated benchmarks and tests

---
+++ inst/benchmarks/bench-xIsPrefixOf.R
@@ -0,0 +1,12 @@
+
+list(
+	one_hundred  = 1:100,
+	one_thousand = 1:1000,
+	ten_thousand = 1:10000
+)
+
+
+
+xIsPrefixOf(one_hundred, one_hundred)
+xIsPrefixOf(one_thousand, one_thousand)
+xIsPrefixOf(ten_thousand, ten_thousand)
