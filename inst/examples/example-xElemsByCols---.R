9d4165c081e75410d845ffc835dfe13c87d1c424
rgrannell1 <r.grannell2@gmail.com>
2015-01-24 12:32:15 +0000

    removed old functions

--- inst/examples/example-xElemsByCols---.R
+++
@@ -1,6 +0,0 @@
-
-# 1. Enumerate the elements of a matrix by its columns.
-
-x_(matrix(1:4, 2, 2)) $ x_ElemsByCols()
-
-# list(1, 2, 3, 4)
--- inst/examples/example-xElemsByRows---.R
+++
@@ -1,4 +0,0 @@
-
-# 1. Enumerate the elements of a matrix by its rows.
-
-x_(matrix(1:4, 2, 2)) $ x_ElemsByRows()
