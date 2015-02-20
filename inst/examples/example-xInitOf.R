1bd51b25e5f1957d8674eb8e55af86ce35fb2b49
rgrannell1 <r.grannell2@gmail.com>
2015-02-20 11:11:20 +0000

    added more examples

--- inst/examples/example-xInitOf.R
+++ inst/examples/example-xInitOf.R
@@ -1,3 +1,15 @@
 
-NULL
+# 1. drop the last element of a toy collection.
 
+xInitOf_(1, 2, 3)
+# list(1, 2)
+
+# 2. reverse a list recursively.
+
+myReverse <- coll := {
+	xJoin_( list(xLastOf(coll)), xReverse(xInitOf(coll)) )
+}
+
+myReverse(list(1, 2, 3, 4))
+
+# list(4, 3, 2, 1)
--- inst/examples/example-xIsInfixOf.R
+++ inst/examples/example-xIsInfixOf.R
@@ -1,2 +1,6 @@
 
-NULL
+# 1. is one word contained within another?
+
+x_('potatoe') $ xToChars() $ xIsInfixOf(coll2 = xToChars('toe'))
+
+# True
